cc7c9d0593bd376a6e90108a3db55f81e0d04c5a
Filmawit Mekonen <zanta.nahu1411@gmail.com>
2022-03-16 21:56:43 +0300

    correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,16 +8,8 @@
 int main(void)
 {
 	int i;
-<<<<<<< HEAD
-	
-	i = 0;
-	positive_or_negative(i);
-	
-=======
 
 	i = 0;
 	positive_or_negative(i);
-
->>>>>>> d6914d42d541750e5a1fe7b5cb54df61a99dfe9a
 	return (0);
 }
