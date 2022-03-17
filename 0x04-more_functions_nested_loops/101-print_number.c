0541c469dec5dca943b69b06aa5cfe22714a6f88
Filmawit Mekonen <zanta.nahu1411@gmail.com>
2022-03-17 23:45:51 +0300

    prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,20 @@
 #include "main.h"
-
 /**
- * print_number - prints number
- *
- * @n: integer to print to character
+ * print_number -print number putchar
+ * @n:integer
+ * Return: void
  */
 void print_number(int n)
 {
-	int i;
-	int d = 1;
 	unsigned int x = n;
-	unsigned int y = n;
-	int c = 0;
 
-	if (n == 0)
-	{
-		_putchar('0');
-	}
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n + 1;
-		n = -n;
-		y = n;
-		x = n;
-		x += 1;
-		y += 1;
+		x = -x;
 	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
 
-	while (x != 0)
-	{
-		x = x / 10;
-		c++;
-	}
-	for (i = 1; i < c; i++)
-	{
-		d *= 10;
-	}
-	for (i = 0; i < c; i++)
-	{
-		_putchar(y / d + '0');
-		y = y % d;
-		d = d / 10;
-	}
+	_putchar(x % 10 + '0');
 }
