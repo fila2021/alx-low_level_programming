ae451883efdf32442b41978f5b2fa369add1c72b
Filmawit Mekonen <zanta.nahu1411@gmail.com>
2022-04-26 23:36:16 +0300

    fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,20 +1,27 @@
-#ifndef LISTS_H_
-#define LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+ */
 
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -32,4 +39,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
